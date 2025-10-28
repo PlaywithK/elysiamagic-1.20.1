ece50041e883fe66db91c33f99c88e6e820f11ec
PlaywithK <playwithk2001@gmail.com>
2025-10-28 07:32:24 +0100

    Initial commit

---
+++ src/main/java/pwk/elysiamagic/ElysiaMagic.java
@@ -0,0 +1,16 @@
+package pwk.elysiamagic;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ElysiaMagic implements ModInitializer {
+	public static final String MOD_ID = "elysiamagic";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
